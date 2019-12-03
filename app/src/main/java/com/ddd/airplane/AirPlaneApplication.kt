6afb8ae64decb4a76553fb75dd4a9ba397331680
SangEun <seun0101@gmail.com>
2019-12-03 18:57:56 +0900

    카카오로그인 기본구조

--- app/src/main/java/com/ddd/airplane/AirPlaneApplication.kt
+++ app/src/main/java/com/ddd/airplane/AirPlaneApplication.kt
@@ -3,6 +3,8 @@ package com.ddd.airplane
 import android.app.Application
 import com.ddd.airplane.common.network.retrofit.RetrofitManager
 import com.ddd.airplane.common.room.DatabaseManager
+import com.ddd.airplane.pages.signin.KakaoSdkAdapter
+import com.kakao.auth.KakaoSDK
 import timber.log.Timber
 
 /**
@@ -12,14 +14,26 @@ class AirPlaneApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        instance = this
         retrofit()
         timber()
         room()
+        kakao()
     }
 
     override fun onTerminate() {
         DatabaseManager.close()
+        instance = null
         super.onTerminate()
+    }
+
+    companion object {
+        var instance: AirPlaneApplication? = null
+    }
+
+    fun getGlobalApplicationContext() : AirPlaneApplication {
+        checkNotNull(instance) {"this application does not inherit"}
+        return instance!!
     }
 
     /**
@@ -43,5 +57,12 @@ class AirPlaneApplication : Application() {
      */
     private fun room() {
         DatabaseManager.init(applicationContext)
+    }
+
+    /**
+     * Kakao
+     */
+    private fun kakao() {
+        KakaoSDK.init(KakaoSdkAdapter())
     }
 }
