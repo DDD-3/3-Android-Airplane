b74233adf59adbd18825a34ef0a493676d0be5e0
SangEun <seun0101@gmail.com>
2019-12-03 20:09:00 +0900

    카카오로 로그인 커스텀 버튼 구현

--- app/src/main/java/com/ddd/airplane/presenter/signin/view/SignInActivity.kt
+++ app/src/main/java/com/ddd/airplane/presenter/signin/view/SignInActivity.kt
@@ -2,7 +2,9 @@ package com.ddd.airplane.presenter.signin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ddd.airplane.R
+import kotlinx.android.synthetic.main.signin_activity.*
 
 /**
  * 로그인
@@ -13,5 +15,9 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_activity)
+        btn_login.setOnClickListener(View.OnClickListener {
+            btn_kakao_login.performClick()
+        })
+
     }
 }
