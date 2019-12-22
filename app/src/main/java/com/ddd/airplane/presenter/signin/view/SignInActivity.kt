5faa84fe31649a36936da96f219e7a49ac208500
SangEun <seun0101@gmail.com>
2019-12-22 17:56:26 +0900

    login 화면 디자인 적용

--- app/src/main/java/com/ddd/airplane/presenter/signin/view/SignInActivity.kt
+++ app/src/main/java/com/ddd/airplane/presenter/signin/view/SignInActivity.kt
@@ -1,9 +1,11 @@
 package com.ddd.airplane.presenter.signin.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.ddd.airplane.R
+import com.ddd.airplane.presenter.main.view.MainActivity
 import kotlinx.android.synthetic.main.signin_activity.*
 
 /**
@@ -15,9 +17,11 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signin_activity)
-        btn_login.setOnClickListener(View.OnClickListener {
-            btn_kakao_login.performClick()
-        })
+        btn_login.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            mainIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(mainIntent)
+        }
 
     }
 }
