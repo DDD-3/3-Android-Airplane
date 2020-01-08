55d64ff283fbb25178bb25520659c6459707fd80
jess <sejin-011@hanmail.net>
2020-01-08 10:59:56 +0900

    로그인 실패 처리, 회원 정보 처리 리팩토링

--- app/src/main/java/com/ddd/airplane/presenter/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/ddd/airplane/presenter/mypage/view/MyPageFragment.kt
@@ -1,13 +1,12 @@
 package com.ddd.airplane.presenter.mypage.view
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.ddd.airplane.R
 import com.ddd.airplane.common.base.BaseFragment
+import com.ddd.airplane.common.manager.MemberManager
 import com.ddd.airplane.databinding.MypageFragmentBinding
 import com.ddd.airplane.presenter.mypage.viewmodel.MyPageViewModel
-import com.ddd.airplane.presenter.signin.view.SignInActivity
 import kotlinx.android.synthetic.main.mypage_fragment.*
 
 /**
@@ -39,7 +38,9 @@ class MyPageFragment : BaseFragment<MypageFragmentBinding, MyPageViewModel>(),
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.cl_profile -> { // 로그인화면 (임시)
-                startActivity(Intent(context, SignInActivity::class.java))
+                MemberManager.signIn(context) {
+
+                }
             }
         }
     }
