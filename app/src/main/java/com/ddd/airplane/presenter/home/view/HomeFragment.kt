62353f5c939c14be8cc6d30f07dedba87a7773f0
jess <sejin-011@hanmail.net>
2020-01-18 09:36:24 +0900

    메인액티비티 레이아웃 리소스 정리, 검색 버튼 구성

--- app/src/main/java/com/ddd/airplane/presenter/home/view/HomeFragment.kt
+++ app/src/main/java/com/ddd/airplane/presenter/home/view/HomeFragment.kt
@@ -3,8 +3,13 @@ package com.ddd.airplane.presenter.home.view
 import android.os.Bundle
 import com.ddd.airplane.R
 import com.ddd.airplane.common.base.BaseFragment
+import com.ddd.airplane.common.utils.DeviceUtils
 import com.ddd.airplane.databinding.HomeFragmentBinding
 import com.ddd.airplane.presenter.home.viewmodel.HomeViewModel
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.home_fragment.*
+import timber.log.Timber
+import kotlin.math.abs
 
 
 /**
@@ -22,12 +27,26 @@ class HomeFragment : BaseFragment<HomeFragmentBinding, HomeViewModel>() {
     }
 
     override fun initLayout() {
-
-
+        abl_home.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (abs(verticalOffset) >= appBarLayout.totalScrollRange) {
+                setStatusBarColor(false)
+            } else {
+                setStatusBarColor(true)
+            }
+        })
     }
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        setStatusBarColor(true)
+    }
 
+    private fun setStatusBarColor(isExpended: Boolean) {
+        val color = if (isExpended) {
+            R.color.color_0cbfff
+        } else {
+            R.color.brand_black
+        }
+        DeviceUtils.setStatusBarColor(activity, color)
     }
 
 }
