4e531ffaa81e3806abce0cf113722fa08e9f6cf5
jess <sejin-011@hanmail.net>
2020-01-22 16:40:26 +0900

    홈 실행에러 수정

--- app/src/main/java/com/ddd/airplane/presenter/home/view/HomeFragment.kt
+++ app/src/main/java/com/ddd/airplane/presenter/home/view/HomeFragment.kt
@@ -28,33 +28,33 @@ class HomeFragment : BaseFragment<HomeFragmentBinding, HomeViewModel>() {
 
     override fun initLayout() {
 
-        // 홈 배너 리스트
-        // viewPager
-        rv_home.adapter =
-
-            object : BaseRecyclerViewAdapter<HomeData, ViewDataBinding>() {
-
-                override fun getItemViewType(position: Int): Int {
-                    return viewModel.getItemViewType(position)
-                }
-
-                override fun onCreateViewHolder(
-                    parent: ViewGroup,
-                    viewType: Int
-                ): BaseViewHolder<HomeData> {
-
-                    val dataBinding = when (Home.Type.values()[viewType]) {
-                        Home.Type.SWIPE_BANNER -> {
-                            createViewDataBinding(parent, R.layout.home_swipe_banner)
-                        }
-
-                        else -> {
-                            createViewDataBinding(parent, R.layout.empty_view)
-                        }
-                    }
-                    return createViewHolder(dataBinding)
-                }
-            }
+//        // 홈 배너 리스트
+//        // viewPager
+//        rv_home.adapter =
+//
+//            object : BaseRecyclerViewAdapter<HomeData, ViewDataBinding>() {
+//
+//                override fun getItemViewType(position: Int): Int {
+//                    return viewModel.getItemViewType(position)
+//                }
+//
+//                override fun onCreateViewHolder(
+//                    parent: ViewGroup,
+//                    viewType: Int
+//                ): BaseViewHolder<HomeData> {
+//
+//                    val dataBinding = when (Home.Type.values()[viewType]) {
+//                        Home.Type.SWIPE_BANNER -> {
+//                            createViewDataBinding(parent, R.layout.home_swipe_banner)
+//                        }
+//
+//                        else -> {
+//                            createViewDataBinding(parent, R.layout.empty_view)
+//                        }
+//                    }
+//                    return createViewHolder(dataBinding)
+//                }
+//            }
 
     }
 
