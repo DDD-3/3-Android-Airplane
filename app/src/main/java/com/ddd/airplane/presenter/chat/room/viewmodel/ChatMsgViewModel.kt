cd80ea0425f83857b5895d356254aaaed2c7ac44
jess <sejin-011@hanmail.net>
2020-02-01 13:29:48 +0900

    bind:textColor 적용

--- app/src/main/java/com/ddd/airplane/presenter/chat/room/viewmodel/ChatMsgViewModel.kt
+++ app/src/main/java/com/ddd/airplane/presenter/chat/room/viewmodel/ChatMsgViewModel.kt
@@ -1,16 +1,15 @@
 package com.ddd.airplane.presenter.chat.room.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.ddd.airplane.R
 import com.ddd.airplane.data.response.chat.ChatMessageData
-import kotlin.properties.Delegates
+import timber.log.Timber
 
 class ChatMsgViewModel {
+
     var messageId: Long? = 0
     var senderId: String = ""
     var content: String = ""
-    var textColor: Int? = 0
+    var textColor: Int? = R.color.brand_white
 
     fun setChatMsg(model: ChatMessageData.MessageData?) {
         model?.let { msg ->
@@ -18,19 +17,48 @@ class ChatMsgViewModel {
             senderId = msg.senderId.toString()
             content = msg.content.toString()
 
-            when (model?.messageId?.rem(11)?.toInt()) {
-                0 -> { textColor = R.color.text_grey }
-                1 -> { textColor = R.color.text_baby }
-                2 -> { textColor = R.color.text_sky }
-                3 -> { textColor = R.color.text_yellow }
-                4 -> { textColor = R.color.text_pink }
-                5 -> { textColor = R.color.text_blue }
-                6 -> { textColor = R.color.text_navy }
-                7 -> { textColor = R.color.text_orange }
-                8 -> { textColor = R.color.text_green }
-                9 -> { textColor = R.color.text_purple }
-                10 -> { textColor = R.color.text_deep }
+            Timber.d(">> ${model.messageId?.rem(11)?.toInt()}")
+            textColor = when (model.messageId?.rem(11)?.toInt()) {
+                0 -> {
+                    R.color.text_grey
+                }
+                1 -> {
+                    R.color.text_baby
+                }
+                2 -> {
+                    R.color.text_sky
+                }
+                3 -> {
+                    R.color.text_yellow
+                }
+                4 -> {
+                    R.color.text_pink
+                }
+                5 -> {
+                    R.color.text_blue
+                }
+                6 -> {
+                    R.color.text_navy
+                }
+                7 -> {
+                    R.color.text_orange
+                }
+                8 -> {
+                    R.color.text_green
+                }
+                9 -> {
+                    R.color.text_purple
+                }
+                10 -> {
+                    R.color.text_deep
+                }
+
+                else -> {
+                    R.color.text_grey
+                }
             }
+
+            Timber.d(">> textColor $textColor")
         }
     }
 }
